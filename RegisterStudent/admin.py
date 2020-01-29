e8c4ad4927618141251969a158fcd56bc54580a4
Tsyon1218 <57703012+Tsyon1218@users.noreply.github.com>
2020-01-30 01:14:32 +0300

    Ethio-Academy

---
+++ RegisterStudent/admin.py
@@ -0,0 +1,3 @@
+from django.contrib import admin
+
+# RegisterStudent your models here.
---
+++ RegisterStudent/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RegisterStudentConfig(AppConfig):
+    name = 'RegisterStudent'
